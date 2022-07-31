3c4b7b835f53a78c44cc7c47c645eb62c90e0841
danielom23-meet <109900040+danielom23-meet@users.noreply.github.com>
2022-07-31 14:34:27 +0300

    sign up and sign in added

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,18 +2,49 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyA4asYj9YmVNUU2w5mEPlC_eC3sUdEs4F0",
+  "authDomain": "test-project-e2c4c.firebaseapp.com",
+  "projectId": "test-project-e2c4c",
+  "storageBucket": "test-project-e2c4c.appspot.com",
+  "messagingSenderId": "494597001768",
+  "appId": "1:494597001768:web:936251f57d8ed2a6211b1d",
+  "measurementId": "G-Y3SM7CLWVN", 
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Error with signing up"
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Error with signing up"
+    return render_template("signup.html", error = error)
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
